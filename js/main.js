44f103c35a97914daef9b71b6bc40a1986ba1d87
drwebmaker <valeriy.abornev@globallogic.com>
2016-03-01 12:53:26 +0200

    added footer

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 /**
  * Created by valeriy.abornyev on 2/28/2016.
  */
-$(".left-sidebar").height($(".content").height() - 28);
+var headerHeight = $("#header").height(),
+    navHeight = $(".navigation").height(),
+    footerHeight = $("#footer").height(),
+    wrapHeight = $(".wrap").height();
+
+$(".left-sidebar").height(wrapHeight - headerHeight - navHeight - footerHeight - 28);
